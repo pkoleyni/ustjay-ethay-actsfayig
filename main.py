82408ecd9c0eaf926540b6e7bf30a6ce42da02aa
Pooria <p.koleyni@gmail.com>
2020-03-08 11:51:41 -0700

    Lesson05

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, redirect
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -13,16 +13,19 @@ def get_fact():
 
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
     return facts[0].getText()
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
-
+    post_url = 'https://hidden-journey-62459.herokuapp.com/piglatinize/'
+    fact = get_fact()
+    payload = {'input_text': fact }
+    user_agent = {'User-Agent': 'Mozilla/5.0'}
+    redirect_url = requests.post(post_url, data=payload, headers = user_agent)
+    return redirect(redirect_url.url, code = 302)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug='True')
 
